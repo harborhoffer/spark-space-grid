78d72df081f7071ed7684114d11990a0466ad8c8
harborhoffer <davejhoff@gmail.com>
2018-03-05 12:16:38 -0500

    Set CLIENT_ID as env variable

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = 'C5c066db70b6fc6cedc64e4ba3046c7148aba71cead9a9e2be03e13aa6aff8a49';
+export const CLIENT_ID = CLIENT_ID;
 export const APP_NAME = "Spark Space Grid";
 export const API_URL = 'https://api.ciscospark.com/v1';
 export const AUTH_URL = 'https://api.ciscospark.com/v1/authorize';
